5947c8e202f1bc9967a23cb0d53f01040196e81c
Vitor Guimarães <vitorguimaraes.ce@gmail.com>
2020-05-19 18:39:49 -0300

    Bug in Postman with Heroku corrected. (Use python 3.7.7)

--- models/store.py
+++ models/store.py
@@ -1,23 +1,24 @@
 from db import db
 
-class StoreModel(db.Model):
-    __tablename__ = 'stores'
 
-    id = db.Column(db.Integer, primary_key=True)
+class StoreModel(db.Model):
+    __tablename__ = "stores"
+    
+    id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(80))
 
-    items = db.relationship('ItemModel', lazy='dynamic')
+    items = db.relationship("ItemModel", lazy = "dynamic")
 
     def __init__(self, name):
         self.name = name
 
     def json(self):
-        return {'name': self.name, 'items': [item.json() for item in self.items.all()]}
-
+        return {"name": self.name, "items": [item.json() for item in self.items.all()]}
+    
     @classmethod
     def find_by_name(cls, name):
-        return cls.query.filter_by(name=name).first()
-
+        return cls.query.filter_by(name=name).first() # "SELECT * FROM items WHERE name=? LIMIT 1"
+        
     def save_to_db(self):
         db.session.add(self)
         db.session.commit()
--- models/user.py
+++ models/user.py
@@ -1,25 +1,28 @@
-import sqlite3
 from db import db
 
 class UserModel(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(80))
     password = db.Column(db.String(80))
 
     def __init__(self, username, password):
         self.username = username
-        self.password = password
-
+        self.password = password 
+    
     def save_to_db(self):
         db.session.add(self)
         db.session.commit()
 
+    # Retrieve user by username
     @classmethod
     def find_by_username(cls, username):
-        return cls.query.filter_by(username=username).first()
-
+        return cls.query.filter_by(username = username).first() # "SELECT * FROM users WHERE username=?"
+    
+    # Retrieve user by id 
     @classmethod
     def find_by_id(cls, _id):
-        return cls.query.filter_by(id=_id).first()
+        return cls.query.filter_by(id = _id).first() # "SELECT * FROM users WHERE id=?"
+        
+        
