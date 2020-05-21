c5e936106413c3d2c6f99349693f2a8c6238a38c
Vitor Guimarães <vitorguimaraes.ce@gmail.com>
2020-05-21 15:07:29 -0300

    Logging with JWT-extended

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask
 from flask_restful import Api
-from flask_jwt import JWT 
+from flask_jwt_extended import JWTManager 
 from datetime import timedelta
 
-from security import authenticate, identity
-from resources.user import UserRegister, User
+from resources.user import UserRegister, User, UserLogin
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
@@ -13,13 +12,12 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 # If the flask extensions raises an error, it can send their own error messages
 app.config['PROPAGATE_EXCEPTIONS'] = True
-app.secret_key = "vitor" # it should be secret
+app.config["JWT_SECRET_KEY"] = "super-secret"
 api = Api(app)
-# Change the url to the authentication endpoint
-# app.config['JWT_AUTH_URL_RULE'] = "/login"
 # Token Expiration Time
 app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=300)
 
+# /auth endpoint (you can change with app.config['JWT_AUTH_URL_RULE'] = "/any"
 jwt = JWT(app, authenticate, identity) # /auth endpoint
 api.add_resource(Item, "/item/<string:name>") 
 api.add_resource(ItemList, "/items")
--- resources/user.py
+++ resources/user.py
@@ -1,23 +1,24 @@
-import sqlite3
 from flask_restful import Resource, reqparse
 from models.user import UserModel
+from werkzeug.security import safe_str_cmp
+from flask_jwt_extended import create_access_token, create_refresh_token
+
+
+_user_parser = reqparse.RequestParser()
+_user_parser.add_argument("username", 
+    type = str,
+    required = True,
+    help = "This field cannot be blank.")
+
+_user_parser.add_argument("password", 
+    type = str,
+    required = True,
+    help = "This field cannot be blank.")
 
 
 class UserRegister(Resource):
-    
-    parser = reqparse.RequestParser()
-    parser.add_argument("username", 
-        type = str,
-        required = True,
-        help = "This field cannot be blank.")
-    
-    parser.add_argument("password", 
-        type = str,
-        required = True,
-        help = "This field cannot be blank.")
-
     def post(self):
-        data = UserRegister.parser.parse_args()
+        data = _user_parser.parse_args()
         username = data["username"]
 
         if UserModel.find_by_username(username):
@@ -43,3 +44,20 @@ class User(Resource):
             return {"message": "User '{}' deleted.".format(user_id)}, 200
 
         return {"message": "User '{}' not found".format(user_id)}, 404
+
+
+class UserLogin(Resource):
+    @classmethod
+    def post(cls):
+        data = _user_parser.parse_args()
+        user = UserModel.find_by_username(data["username"])
+
+        if user and safe_str_cmp(user.password, data["password"]):
+            access_token = create_access_token(identity = user.id, fresh = True)
+            refresh_token = create_refresh_token(user.id)
+            return {
+                "access_token": access_token,
+                "refresh_token": refresh_token
+            }, 200
+        
+        return {"message": "Invalid credentials."}, 401
